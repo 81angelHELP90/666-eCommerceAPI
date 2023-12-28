2fe24a3f146018f8392d3d44d4dca9a11947209d
81angelHELP98 <vangel338@gmail.com>
2023-12-27 22:44:32 -0300

    agrego fetch, modal, carrito de compras

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,6 @@
 function getDataAPI(){
     const urlProd = "https://fakestoreapi.com/products";
+    //const urlProd = "https://api.escuelajs.co/api/v1/products";
     
     fetch(urlProd)
         .then(response => {
@@ -9,7 +10,6 @@ function getDataAPI(){
                 throw new Error("Error fetch");
         })
         .then(data => {
-            //console.log(data);
             bindingData(data);
         })
         .catch(error => {
@@ -18,19 +18,67 @@ function getDataAPI(){
 }
 
 function bindingData(items) {
-    let Imgs = document.getElementsByClassName("card");
+    let mySection = document.getElementById("imgSection");
+    localStorage.setItem("itemCounter", 0);
+    localStorage.setItem("addMsg", true);
+    localStorage.setItem("itemsAndCant", "");
 
-    let img = document.getElementById("img1");
+    items.forEach(item => {
+        let divCard = document.createElement("div");
+        divCard.className = "card";
 
-    img.src = items[0].image; //"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+        mySection.appendChild(divCard);
 
-    /*
-    for(let i=0; Imgs.length < i; i++){
-        Imgs[i].children[0].src = items[i].image;
-    }
-    */
+        //img dentro del div Padre:
+        let imgCard = document.createElement("img");
+        imgCard.className = "imgCard";
+        imgCard.src = (item.image) ? item.image : item.images[0];
 
-    //Imgs[0].children[0].src = items[0].image;
+        imgCard.addEventListener("click", function(){
+            navDetail(item.id, items);
+        });
+
+        //div dentro del div Padre:
+        let bodyCard = document.createElement("div");
+        bodyCard.className = "card-body";
+
+        //h5 dentro del div2:
+        let h5Body = document.createElement("h5");
+        h5Body.className = "card-title";
+        h5Body.innerHTML = item.title;
+
+        //p dentro del div2:
+        let pBody = document.createElement("p");
+        pBody.className = "card-text";
+        pBody.innerHTML = item.description;
+
+        //Footter 
+        let footerCard = document.createElement("div");
+        footerCard.className = "footter";
+
+        let btnAgregar = document.createElement("button");
+        btnAgregar.className = "btn btn-primary cartBtn";
+        btnAgregar.innerHTML = "Agregar";
+        btnAgregar.addEventListener("click", function(){
+            listItem(items, item.id, false);
+        });
+
+        //Buttom dentro del footter
+        let btnEliminar = document.createElement("button");
+        btnEliminar.className = "btn btn-secondary cartBtn";
+        btnEliminar.innerHTML = "Eliminar";
+        btnEliminar.addEventListener("click", function(){
+            listItem(items, item.id, true);
+        });
+
+        divCard.appendChild(bodyCard);
+        bodyCard.appendChild(h5Body);
+        divCard.appendChild(imgCard);
+        divCard.appendChild(pBody);
+        divCard.appendChild(footerCard);
+        footerCard.appendChild(btnAgregar);
+        footerCard.appendChild(btnEliminar);
+    });
 }
 
 getDataAPI();
